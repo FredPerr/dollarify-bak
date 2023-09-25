a6756be94084ea5384af9fd3508d82fc459f5dbb
Frédéric Perron <frederic.perr@gmail.com>
2023-09-25 15:50:48 -0400

    refactor: clear all impl

--- packages/core/src/lib/utils/format.ts
+++
@@ -1,9 +0,0 @@
-type ReplacementsType = { [key: string]: string };
-
-function format(str: string, replacements: ReplacementsType): string {
-  for (const key in Object.keys(replacements))
-    str = str.replace(key.toString(), replacements[key]);
-  return str;
-}
-
-export default format;
